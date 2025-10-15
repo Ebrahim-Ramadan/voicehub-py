6b90f095a64696b4bfdfb5024d274e810726e323
Ebrahim-Ramadan <ramadanebrahim791@gmail.com>
2025-10-15 08:25:03 +0200

    ass

--- main.py
+++ main.py
@@ -470,4 +470,4 @@ async def startup_event():
     app.state.websockets = set()
 
 if __name__ == "__main__":
-    uvicorn.run("ass:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
